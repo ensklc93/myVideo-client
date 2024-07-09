b009b51119e69d2b7a547c660b5ba53e565b5a84
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-09 15:40:48 +0200

    Added PropTypes
    Added PropTypes to ensure right format of fetched API Data

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,27 @@
+import PropTypes from "prop-types"
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
       onClick={() => {
-        onMovieClick(movie);
+        onMovieClick(movie)
       }}
     >
-        {movie.title}
+      {movie.title}
     </div>
-  );
-};
+  )
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    directorName: PropTypes.string.isRequired,
+    genreName: PropTypes.string.isRequired,
+    genreDescription: PropTypes.string.isRequired,
+    directorBio: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+}
+
