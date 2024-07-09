cd435843830eed1ec80a620a0c7624fef73300b5
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-09 15:39:47 +0200

    Added syntax to fetch api from heroku

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card.jsx";
 import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-movie-app-ab91e4bb4611.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data check:", data)
+        const moviesFromApi = data.map((movie) => {
+          return {
+            id: movie._id,
+            title: movie.Title,
+            description: movie.Description,
+            genreName: movie.Genre.Name,
+            genreDescription: movie.Genre.Description,
+            directorName: movie.Director.Name,
+            directorBio: movie.Director.Bio,
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
