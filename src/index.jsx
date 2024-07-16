9a1ebdbb5ab6ff8013cbe990f4ec3b57c37b8145
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-16 18:12:17 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client"
-import { MainView } from "./components/main-view/main-view.jsx"
+import { MainView } from "./components/main-view/main-view"
 import Container from 'react-bootstrap/Container';
 
 import "bootstrap/dist/css/bootstrap.min.css"
