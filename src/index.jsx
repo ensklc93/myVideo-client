c1c62a5aee51c5c484b4b2a8ed95b049b51be82f
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-23 20:29:51 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client"
 import { MainView } from "./components/main-view/main-view"
 import Container from 'react-bootstrap/Container';
 
+
 import "bootstrap/dist/css/bootstrap.min.css"
 // Import statement to indicate that it needs to bundle `./index.scss`
 import "./index.scss"
