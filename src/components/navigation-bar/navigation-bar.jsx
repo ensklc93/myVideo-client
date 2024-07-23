7a8db78721a14d48733ed5c5072c52657d12dd63
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-23 20:29:48 +0200

    Added the correct name and property
    Added the user.username property to Navbar/Profile

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -6,12 +6,13 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Books App
+          Movie App
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {!user && (
+            
               <>
                 <Nav.Link as={Link} to="/login">
                   Login
@@ -25,6 +26,9 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               <>
                 <Nav.Link as={Link} to="/">
                   Home
+                </Nav.Link>
+                <Nav.Link as={Link} to={`/users/${user.username}`}>
+                  Profile
                 </Nav.Link>
                 <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
               </>
