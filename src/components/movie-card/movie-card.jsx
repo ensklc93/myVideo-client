e0a4bf5d3790cbdb29288d3ef32129edb247a98d
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-07-23 13:38:53 +0200

    Added Button inside Bootstrap Link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import PropTypes from "prop-types"
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
       <Card.Img variant="top" src="https://placehold.co/300x300" />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.description}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="outline-success">
-          Open
-        </Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
